51f244888d73251e15432a71626143d3d708d0a9
Bilel KHARBECHE <bilelkharbeche71@gmail.com>
2019-04-12 15:47:44 +0200

    5e commit

--- src/main/java/dev/App.java
+++ src/main/java/dev/App.java
@@ -1,4 +1,6 @@
 package dev;
+
+import java.util.ResourceBundle;
 import com.github.lalyos.jfiglet.FigletFont;
 
 /**
@@ -7,9 +9,15 @@ import com.github.lalyos.jfiglet.FigletFont;
  */
 
 public class App {
+	
 	  public static void main(String[] args) {
-	    String asciiArt = FigletFont.convertOneLine("T Mauche !:D");
+	    String titre = ResourceBundle.getBundle("application").getString("titre");
+	    String asciiArt = FigletFont.convertOneLine(titre);
 	    System.out.println(asciiArt);
+	    
+	    // affichage de l'environnement
+	    String environnement = ResourceBundle.getBundle("application").getString("environnement");
+	    System.out.println("Environnement : " + environnement);
 	  }
     
 }
